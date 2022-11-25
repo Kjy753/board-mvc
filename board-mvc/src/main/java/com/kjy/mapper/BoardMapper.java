1b33534bf2ed0224d64833e4f6911305e2076391
kjy <aty753@naver.com>
2022-11-26 02:25:47 +0900

    insert 관련 2가지 처리

--- board-mvc/src/main/java/com/kjy/mapper/BoardMapper.java
+++ board-mvc/src/main/java/com/kjy/mapper/BoardMapper.java
@@ -11,6 +11,9 @@ public interface BoardMapper {
 	
 	public List<BoardVO> getList();
 	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
 	
 
 }
--- board-mvc/src/test/java/com/kjy/mapper/BoardMapperTests.java
+++ board-mvc/src/test/java/com/kjy/mapper/BoardMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.kjy.domain.BoardVO;
+
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,6 +23,32 @@ public class BoardMapperTests {
 		
 		log.info("--------------------");
 		boardMapper.getList();
+	}
+	
+	@Test
+	public void testInster() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("test 테스트");
+		board.setContent("Content 테스트");
+		board.setWriter("tester");
+		
+		boardMapper.insert(board);
+		log.info("=========================");
+		log.info("after insert " + board.getBno());
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("select 테스트");
+		board.setContent("select 테스트");
+		board.setWriter("tester");
+		
+		boardMapper.insertSelectKey(board);
+		log.info("=========================");
+		log.info("after insert selectKey" + board.getBno()); 
 	}
 }
 
